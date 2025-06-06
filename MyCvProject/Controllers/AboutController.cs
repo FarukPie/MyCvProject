74940e46daccfd882d4987151118007264bf60f9
Lenovo <Lenovo@LAPTOP-GCC8HOTA>
2025-06-06 17:11:44 +0300

    added dynamic about section

--- MyCvProject/Controllers/AboutController.cs
+++ MyCvProject/Controllers/AboutController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyCvProject.Models.Entity;
+using MyCvProject.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,52 @@ namespace MyCvProject.Controllers
 {
     public class AboutController : Controller
     {
-        // GET: About
+        AboutRepository repo = new AboutRepository();
         public ActionResult Index()
         {
+            var abouts = repo.List();
+            return View(abouts);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddAbout(TblAbout p)
+        {
+            repo.Tadd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteAbout(int id)
+        {
+            TblAbout t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult GetAbout(int id)
+        {
+            TblAbout t = repo.Find(x => x.ID == id);
+            return View(t);
+
+        }
+
+        [HttpPost]
+        public ActionResult GetAbout(TblAbout p)
+        {
+            TblAbout t = repo.Find(x => x.ID == p.ID);
+            t.Name = p.Name;
+            t.Surname = p.Surname;
+            t.Adress = p.Adress;
+            t.Phone = p.Phone;
+            t.Mail = p.Mail;
+            t.Description = p.Description;
+            t.ImgUrl = p.ImgUrl;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+
         }
     }
 }
